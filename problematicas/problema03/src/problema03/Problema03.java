f63990308c0040e6fcc012f33429ef52bf34f135
erick <ericktole852@gmail.com>
2023-12-07 16:58:09 -0500

    problema03

--- problematicas/problema03/src/problema03/Problema03.java
+++ problematicas/problema03/src/problema03/Problema03.java
@@ -23,7 +23,7 @@ public class Problema03 {
             //formula para sacar el resultado para la comvercion 
             celsius = (fahrenheit - 32) * 5 / 9;
 
-            System.out.printf("%d.Fahrenheit: %d Celsius: %d\n", i,fahrenheit, celsius);
+            System.out.printf("%d. %d Grados Fahrenheit = %d Grados Celsius\n", i,fahrenheit, celsius);
 
             // Incrementamos el valor de Fahrenheit
             fahrenheit = fahrenheit + 4;
